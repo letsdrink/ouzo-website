da0cbec6325f0a194b3dfde28dfaab0beb015738
piotrooo <piotroo89@gmail.com>
2015-01-09 19:18:38 +0100

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo "deploying";
-curl --ftp-create-dirs -T index.php -u bbankowski_ouzo:ouZO777^ ftp://ftp.bbankowski.nazwa.pl/index.php
+curl --ftp-create-dirs -T index.php -u 'bbankowski_ouzo:ouZO777^' ftp://ftp.bbankowski.nazwa.pl/index.php
 
