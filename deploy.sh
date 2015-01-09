06559614a18808f90d977eaaf8320d33f0237754
piotrooo <piotroo89@gmail.com>
2015-01-09 17:25:30 +0100

    Fix

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-curl --ftp-create-dirs -T uploadfilename -u $FTP_USER:$FTP_PASSWORD ftp://ftp.bbankowski.nazwa.pl
+find . -type f -exec curl -u xxx:psw --ftp-create-dirs -T {} ftp://ftp.bbankowski.nazwa.pl/{} \;
